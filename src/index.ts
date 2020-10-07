5216fb34a4cd949b422aa4c1081bb56987ddc1b9
Daniel Leskosky <dleskosky@gmail.com>
2020-10-07 19:55:51 -0400

    Commit for exercise 9.10

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import diagnosisRouter from './routes/diagnoses';
 
 const app = express();
 // eslint-disable-next-line @typescript-eslint/no-unsafe-call
@@ -12,6 +13,8 @@ app.get('/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosisRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
---
+++ src/services/diagnosisService.ts
@@ -0,0 +1,11 @@
+import diagnoses from '../../data/diagnoses';
+
+import { Diagnosis } from '../types';
+
+const getDiagnoses = (): Array<Diagnosis> => {
+  return diagnoses;
+};
+
+export default {
+  getDiagnoses
+};
