1ae669781e401f1ed4bc74636e6b40493695120f
Daniel Leskosky <dleskosky@gmail.com>
2020-10-07 15:20:40 -0400

    Commit for exercise 9.9

--- build/index.js
+++ build/index.js
@@ -4,9 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const app = express_1.default();
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors_1.default());
 app.use(express_1.default.json());
-const PORT = 3000;
+const PORT = 3001;
 app.get('/ping', (_req, res) => {
     console.log('someone pinged here');
     res.send('pong');
