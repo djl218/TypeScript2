e4f4931b29b9ddbdcad37475f7b9ca661f8b5723
Daniel Leskosky <dleskosky@gmail.com>
2020-10-17 18:57:06 -0400

    Got rid of testing route

--- src/index.ts
+++ src/index.ts
@@ -10,11 +10,6 @@ app.use(express.json());
 
 const PORT = 3001;
 
-app.get('/ping', (_req, res) => {
-  console.log('someone pinged here');
-  res.send('pong');
-});
-
 app.use('/api/diagnoses', diagnosisRouter);
 
 app.use('/api/patients', patientRouter);
